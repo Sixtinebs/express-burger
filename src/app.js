6b05a1e33a201dd636c4982b3940f5b525828009
Sixtine BS <sixtine.bs11@gmail.com>
2022-03-22 19:46:01 +0100

    Add route for create

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const db = require('../scripts/create-db')
+const createBuger = require('./routes/create');
 
 app.use(express.urlencoded({extended:false}));
+app.use(express.static('css'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Si ça ne marche pas avec pug 
-//app.engine('pug', require('pug').__express);
-
+//PUG
+app.engine('pug', require('pug').__express);
 app.set('views', path.resolve(__dirname, 'views'));
-
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {  res.send('Hello, world!');});
+//ROUTES
+app.get('/', (req, res) => {  
+    res.render("index", { title: 'Welcome to Buger Express !!'});
+});
+
+//Route pour créer un nouveau burger et un nouvelle boisson
+app.use('/create', createBuger)
+
 
 module.exports = app;
 
---
+++ src/routes/create.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const db = require('../../scripts/create-db');
+const router = express.Router();
+const multer = require("../middlewar/multer");
+
+router.get('/', (req, res, next) => {
+    const sql = "SELECT * FROM boisson";
+    let boissons = null;
+    let burgers = null;
+    db.all(sql, [], (err, rows) => {
+        if(err) {
+            return console.error(err.message)
+        }
+        boissons = rows;
+        //res.render("create", { boissons: rows})
+    });
+    const sql2 = "SELECT * FROM burger";
+    db.all(sql2, [], (err, rows) => {
+        if(err) {
+            return console.error(err.message)
+        }
+        burgers = rows;
+        //res.render("create", { burgers: rows})
+        res.render("create", { burgers: burgers, boissons: boissons} )
+    });
+
+   
+});
+
+router.post('/new-burger', (req, res, next) => {
+    const sql =  'INSERT INTO burger (nom, prix, description) VALUES ( ?, ?, ?)';
+    const burger = [
+        req.body.nom,
+        req.body.prix,
+        req.body.description
+    ];
+    db.run(sql, burger, err => {
+        if(err){
+            console.log(err);
+        }
+        res.render("index");
+    })
+});
+
+router.post('/new-boisson', multer,(req, res, next) => {
+    const sql =  'INSERT INTO boisson (nom, prix, description, photoURL) VALUES ( ?, ?, ?, ?)';
+    const image = (req.file ? `${req.protocol}://${req.get('host')}/images/${req.file.filename}` : null);
+    const boisson = [
+        req.body.nom,
+        req.body.prix,
+        req.body.description,
+        image
+    ];
+    db.run(sql, boisson, err => {
+        if(err){
+            console.log(err);
+        }
+        res.render("index");
+    })
+});
+
+router.post('/new-menu', multer, (req, res, next) => {
+    const sql = "INSERT INTO menu (nom, accompagnement, description, prix, photoURL, burgerId, boissonId) VALUES (?,?,?,?,?,?,?)";
+    const image = (req.file ? `${req.protocol}://${req.get('host')}/images/${req.file.filename}` : null);
+    const menu = [
+        req.body.nom,
+        req.body.accompagnement,
+        req.body.description,
+        req.body.prix,
+        image,
+        req.body.burger,
+        req.body.boisson
+    ];
+    db.run(sql, menu, err => {
+        if(err){
+            console.log(err);
+        }
+        res.render("index");
+    })
+})
+
+module.exports = router;
